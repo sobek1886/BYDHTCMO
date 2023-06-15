91d80953434b85a62edfa51fb8e30d0e4726a00d
sobek1886 <piotr.sobecki17@gmail.com>
2023-06-15 08:40:34 +0000

    model trained for 80 epochs

---
+++ experiments/GAICD_PA_CP_repeat9/BBox_adjusting.py
@@ -0,0 +1,156 @@
+import numpy as np
+from PIL import Image
+from super_gradients.common.object_names import Models
+from super_gradients.training import models
+import torch
+
+class BoundingBox:
+    def __init__(self, coordinates):
+        self.coordinates = np.array(coordinates, dtype=np.float32)
+
+    def calculate_area(self):
+        width = self.coordinates[2] - self.coordinates[0]
+        height = self.coordinates[3] - self.coordinates[1]
+        return width * height
+
+    def get_coordinates(self):
+        return self.coordinates
+
+class RegionDetector:
+    def __init__(self, image, make_square):
+         self.input_image = image
+         self.make_square = make_square
+         if type(image) == str:  # input_image is a URL
+            self.image = Image.open(image).convert('RGB')
+         elif isinstance(image, Image.Image): # input_image is already a PIL Image object
+            self.image = image
+         else:  # input_image is a numpy ndarray # for gradio
+            self.image = Image.fromarray(image)
+
+         self.image_width, self.image_height = self.image.size
+
+    def YOLO_prediction(self):
+        device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+        model = models.get("yolo_nas_l", pretrained_weights="coco").to(device)
+        image_prediction = list(model.predict(self.input_image, conf=0.2))[0]
+        return image_prediction
+
+    def detect_objects(self, YOLO_predictions):
+        detected_objects = []
+        image_prediction = YOLO_predictions
+        class_names = image_prediction.class_names
+        labels = image_prediction.prediction.labels
+        confidence = image_prediction.prediction.confidence
+        bboxes = image_prediction.prediction.bboxes_xyxy
+
+        for i, (label, conf, bbox) in enumerate(zip(labels, confidence, bboxes)):
+          detected_objects.append(BoundingBox(bbox))
+
+        return detected_objects
+
+    def determine_objects_in_region(self, detected_objects, most_important_region):
+        objects_in_region = []
+        for obj in detected_objects:
+            intersection_area = self.calculate_intersection_area(obj, most_important_region)
+            object_area = obj.calculate_area()
+
+            if intersection_area >= 0.3 * object_area:
+                objects_in_region.append(obj)
+        return objects_in_region
+
+    def calculate_intersection_area(self, obj, region):
+        intersection = np.maximum(0, np.minimum(obj.coordinates, region.coordinates))
+        return BoundingBox(intersection).calculate_area()
+      
+    def adjust_most_important_region(self, objects_in_region, most_important_region):
+      if len(objects_in_region) == 0:
+        min_x, min_y, max_x, max_y = most_important_region.coordinates
+      elif len(objects_in_region) == 1:
+        min_x = min(most_important_region.coordinates[0], objects_in_region[0].coordinates[0])
+        min_y = min(most_important_region.coordinates[1], objects_in_region[0].coordinates[1])
+        max_x = max(most_important_region.coordinates[2], objects_in_region[0].coordinates[2])
+        max_y = max(most_important_region.coordinates[3], objects_in_region[0].coordinates[3])
+      else:
+        '''min_x = min(*([obj.coordinates[0] for obj in objects_in_region]))
+        min_y = min(*([obj.coordinates[1] for obj in objects_in_region]))
+        max_x = max(*([obj.coordinates[2] for obj in objects_in_region]))
+        max_y = max(*([obj.coordinates[3] for obj in objects_in_region]))'''
+        min_x = min(most_important_region.coordinates[0], *([obj.coordinates[0] for obj in objects_in_region]))
+        min_y = min(most_important_region.coordinates[1], *([obj.coordinates[1] for obj in objects_in_region]))
+        max_x = max(most_important_region.coordinates[2], *([obj.coordinates[2] for obj in objects_in_region]))
+        max_y = max(most_important_region.coordinates[3], *([obj.coordinates[3] for obj in objects_in_region]))
+      
+      if self.make_square:
+        width = max_x - min_x
+        height = max_y - min_y
+
+        if width == height:
+          adjusted_region = np.array([
+          max(0, min_x),
+          max(0, min_y),
+          min(max_x, self.image_width),
+          min(max_y, self.image_height)
+          ], dtype=np.float32)
+
+        if width > height:
+          gap = width - height
+          top_available = min_y
+          bot_available = (self.image_height - max_y)
+          if gap >= (top_available + bot_available):
+              adjusted_region = np.array([min_x, 0, max_x, self.image_height], dtype=np.float32)
+              '''if new_height != new_width
+                  outpaint'''
+          else:
+              top_available = min_y
+              bot_available = (self.image_height - max_y)
+              if (top_available < gap/2):
+                  adjusted_region = np.array([min_x, 0, max_x, (self.image_height - (gap - top_available))], dtype=np.float32)
+              elif (bot_available < gap/2):
+                  adjusted_region = np.array([min_x, (gap - bot_available), max_x, (self.image_height)], dtype=np.float32)
+              else:
+                  adjusted_region = np.array([min_x, (min_y - gap/2), max_x, (max_y + gap/2)], dtype=np.float32)
+
+        else:
+          gap = height - width
+          top_available = min_y
+          bot_available = (self.image_height - max_y)
+          if gap >= (top_available + bot_available):
+              adjusted_region = np.array([min_x, 0, max_x, self.image_height], dtype=np.float32)
+              '''if new_height != new_width
+                  outpaint'''
+          else:
+              top_available = min_y
+              bot_available = (self.image_height - max_y)
+              if (top_available < gap/2):
+                  adjusted_region = np.array([min_x, 0, max_x, (self.image_height - (gap - top_available))], dtype=np.float32)
+              elif (bot_available < gap/2):
+                  adjusted_region = np.array([min_x, (gap - bot_available), max_x, (self.image_height)], dtype=np.float32)
+              else:
+                  adjusted_region = np.array([min_x, (min_y - gap/2), max_x, (max_y + gap/2)], dtype=np.float32)
+
+      else:
+        adjusted_region = np.array([
+        max(0, min_x),
+        max(0, min_y),
+        min(max_x, self.image_width),
+        min(max_y, self.image_height)
+        ], dtype=np.float32)
+
+      return adjusted_region
+        
+if __name__ == '__main__':
+  # Usage example:
+  #6
+  most_important_region = [25, 19, 640, 480]
+  #7
+  #most_important_region = [81, 61, 1003, 753]
+  image = "/content/6.png"
+  make_square = True
+
+  region_detector = RegionDetector(image, make_square)
+  YOLO_predictions = region_detector.YOLO_prediction()
+  detected_objects = region_detector.detect_objects(YOLO_predictions)
+  most_important_region = BoundingBox(most_important_region)
+  objects_in_region = region_detector.determine_objects_in_region(detected_objects, most_important_region)
+  adjusted_region = region_detector.adjust_most_important_region(objects_in_region, most_important_region)
+  print("Adjusted region:", adjusted_region)
---
+++ experiments/GAICD_PA_CP_repeat9/outpaint.py
@@ -0,0 +1,190 @@
+import inspect
+from typing import List, Optional, Union
+
+import numpy as np
+import torch
+import PIL
+from PIL import Image, ImageFilter
+import gradio as gr
+from diffusers import StableDiffusionInpaintPipeline
+from SSIM_PIL import compare_ssim
+from config_GAICD import cfg
+
+class OutpaintingFeature:
+    def __init__(self, image, num_samples = 1):
+
+      self.desired_size = cfg.image_size
+      self.outpaint_size = cfg.outpaint_size
+
+      self.input_image = image
+      if type(self.input_image) == str:  # input_image is a URL
+          self.input_image = Image.open(self.input_image)
+      elif isinstance(self.input_image, Image.Image): # input_image is already a PIL Image object
+          pass
+      else:  # input_image is a numpy ndarray # for gradio
+          self.input_image = Image.fromarray(self.input_image)
+      # resize image
+      #self.input_image.thumbnail((512,512))
+      print(f'input to outpaint.py {self.input_image.size}')
+      self.input_image.thumbnail(self.outpaint_size)
+      print(f'after thumbnail in outpaint init {self.input_image.size}')
+
+      # get width and size
+      self.image_width, self.image_height = self.input_image.size
+      # Calculate the size of the new square image
+      self.new_size = max(self.image_width, self.image_height)
+
+      # coordinates used to paste original image back
+      self.x = (self.new_size - self.image_width) // 2
+      self.y = (self.new_size - self.image_height) // 2
+
+      # used to get areas for SSIM
+      self.use_SSIM = True
+      if self.image_width > self.image_height:
+        self.ratio = self.outpaint_size[0]/self.image_width
+        self.wider = True
+        if self.y < 7:
+          self.use_SSIM = False
+      else:
+        self.ratio = self.outpaint_size[0]/self.image_height
+        self.wider = False
+        if self.x < 7:
+          self.use_SSIM = False
+
+      self.device = "cuda"
+      self.model_path = "runwayml/stable-diffusion-inpainting"
+      self.pipe = StableDiffusionInpaintPipeline.from_pretrained(
+          self.model_path,
+          torch_dtype=torch.float16,
+      ).to(self.device)
+      self.generator = torch.Generator(device="cuda").manual_seed(0) # change the seed to get different results
+      self.generation_size = self.outpaint_size[0]
+      ## empty prompt for random generation
+      self.prompt = ""
+      self.seed = 51
+      self.guidance_scale= 7.5
+      self.num_samples = num_samples
+      # negative prompt to steer away the random generation from generating frames/fonts/writings
+      self.negative = '''frame, album cover, document photo, portrait, picture frame, incoherents, collage, type design, magazine cover, with text, cover, painting, wall mural, poster on wall, poster, screenshot, awful, wallpaper, grid, collage, text, writing, with writing, painting on the wall, poster, movie poster, logo, logos, watermark, plate, border, edge, wood, table, fabric, plate, pattern, lowres, error, cropped, worst quality, low quality, jpeg artifacts, out of frame, watermark, signature, deformed, ugly, mutilated, disfigured, text, extra limbs, face cut, head cut, extra fingers, extra arms, poorly drawn face, mutation, bad proportions, cropped head, malformed limbs, mutated hands, fused fingers, long neck'''
+      """frame, album cover, document photo, portrait, picture frame, incoherents, collage, type design, magazine cover, with text, cover, painting, wall mural, poster on wall, poster, screenshot, awful, wallpaper, grid, collage, text, writing, with writing, painting on the wall, poster, movie poster, logo, logos, watermark, plate, border, edge, wood, table, fabric, plate, pattern"""
+
+    def outpaint_image(self):
+
+        #get image with added whitespace and mask
+        squared_image, mask_image = self.get_masks()
+        print(f'whitespaced: {squared_image.size} \n mask{mask_image.size}')
+
+        #when not using SSIM there is no way to compare the outpaints, therefore only 1 is generated
+        if not self.use_SSIM:
+          self.num_samples = 1
+
+        # generate outpaints
+        images = self.pipe(
+            prompt = self.prompt,
+            image = squared_image,
+            mask_image = mask_image,
+            guidance_scale = self.guidance_scale,
+            generator = self.generator,
+            num_images_per_prompt = self.num_samples,
+            width = self.generation_size,
+            height = self.generation_size,
+            negative_prompt = self.negative).images
+
+        if self.use_SSIM:
+          # get areas for SSIM comparison
+          new_areas = [self.get_areas_new(outpainted_image) for outpainted_image in images]
+          og_A, og_B = self.get_areas_original()
+          best_A, idx_best_B = self.get_best_outpaints(og_A, og_B, new_areas)
+
+          #combine the original with the best outpaints
+          result = images[idx_best_B]
+          result.paste(best_A, (0, 0))
+          result.paste(self.input_image, (self.x, self.y))
+          #result.thumbnail(self.desired_size)
+          return result
+
+        else:
+          result = images[0]
+          result.paste(self.input_image, (self.x, self.y))
+          #result.thumbnail(self.desired_size)
+          return result
+
+    def get_masks(self):
+        # Create a new blank square image
+        blank = Image.new("RGB", (self.new_size, self.new_size), (255, 255, 255))
+        # Paste the original image into the center of the new image
+        blank.paste(self.input_image, (self.x, self.y))
+        squared_image = blank
+
+        mask_image = self.input_image.copy().convert('L')
+        mask_image = mask_image.point(lambda x: 0, '1')
+        # Create a new blank square image
+        blank = Image.new("L", (self.new_size, self.new_size), (255))
+        # Paste the mask
+        blank.paste(mask_image, (self.x, self.y))
+        mask_image = blank
+
+        return squared_image, mask_image
+    
+    def get_areas_new(self, outpainted_image):
+
+        outpainted_image_width, outpainted_image_height = outpainted_image.size
+        if self.wider:
+
+            crop_coordinates_new_A = [coord * self.ratio for coord in [0, 0, outpainted_image_width, self.y]]
+            new_area_A = outpainted_image.crop(crop_coordinates_new_A)
+
+            crop_coordinates_new_B = [coord * self.ratio for coord in [0, outpainted_image_height - self.y, outpainted_image_width, outpainted_image_height]]
+            new_area_B = outpainted_image.crop(crop_coordinates_new_B)  
+
+
+        else:
+            crop_coordinates_new_A = [coord * self.ratio for coord in [0, 0, self.x, self.image_height]]
+            new_area_A = outpainted_image.crop(crop_coordinates_new_A)
+
+            crop_coordinates_new_B = [coord * self.ratio for coord in [outpainted_image_width - self.x, 0, outpainted_image_width, outpainted_image_height]]
+            new_area_B = outpainted_image.crop(crop_coordinates_new_B)  
+
+        return [new_area_A, new_area_B]
+
+    def get_areas_original(self):
+      
+      if self.wider:
+        crop_coordinates_A = [coord * self.ratio for coord in [0, 0, self.image_width, self.y]]
+        original_area_A = self.input_image.crop(crop_coordinates_A)
+
+        crop_coordinates_B = [coord * self.ratio for coord in [0, self.image_height - self.y, self.image_width, self.image_height]]
+        original_area_B = self.input_image.crop(crop_coordinates_B)
+
+      else:
+          crop_coordinates_A = [coord * self.ratio for coord in [0, 0, self.x, self.image_height]]
+          original_area_A = self.input_image.crop(crop_coordinates_A)
+
+          crop_coordinates_B = [coord * self.ratio for coord in [self.image_width - self.x, 0, self.image_width, self.image_height]]
+          original_area_B = self.input_image.crop(crop_coordinates_B)
+
+      return original_area_A, original_area_B
+    
+    def get_best_outpaints(self, og_A, og_B, new_areas):
+      SSIM_values_A = []
+      SSIM_values_B = []
+
+      for area in new_areas:
+        print(f'\n og_A: {og_A.size}\n area0: {area[0].size}')
+        value_A = compare_ssim(og_A, area[0], GPU=False)
+        SSIM_values_A.append(value_A)
+
+        value_B = compare_ssim(og_B, area[1], GPU=False)
+        SSIM_values_B.append(value_B)
+
+      idx_A = np.argmax(SSIM_values_A)
+      idx_B = np.argmax(SSIM_values_B)
+
+      return new_areas[idx_A][0], idx_B
+
+if __name__ == '__main__':
+    img_url = "/content/Fork-Human-Centric-Image-Cropping/GAIC_280712.jpg"
+    Outpainter = OutpaintingFeature(img_url, num_samples = 3)
+    output = Outpainter.outpaint_image()
+    output.show()
+
